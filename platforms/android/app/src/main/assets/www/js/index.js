eb039495617bc699e2ff5d7ccf8084fcdb43731f
Chinmay <chinmaydash11@gmail.com>
2021-12-07 13:21:10 +0530

    added comments

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -10,27 +10,23 @@ function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
 
-    setTimeout(function(){
-        cordova.exec(success,null,"CordovaFitnessPlugin","coolMethod",[111,222]);
-    },5000);
+
 
     document.getElementById('loadVisitApp').addEventListener('click',loadVisitURL);
 }
 
-function success(result){
-    alert(result);
-}
 
 
 function loadVisitURL(){
-    console.log("loadVisitURL called");
+    //console.log("loadVisitURL called");
+
     cordova.exec(null,
     null,
-    "CordavaFitnessPlugin",
-    "loadVisitWebUrl",
-    ["https://star-health.getvisitapp.xyz/",
-    "967914547335-g2ntga70t1i7b19ti91gcubb7agm7rje.apps.googleusercontent.com",
-    "Bearer%20eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOi[%E2%80%A6]GFsIn0.f0656mzmcRMSCywkbEptdd6JgkDfIqN0S9t-P1aPyt8",
-    "8158"]
+    "CordavaFitnessPlugin", //plugin class name
+    "loadVisitWebUrl", //plugin method
+    ["https://star-health.getvisitapp.xyz/", //base url (should change based on the build type)
+    "967914547335-g2ntga70t1i7b19ti91gcubb7agm7rje.apps.googleusercontent.com", //firebase default_client_id (should change based on the build type)
+    "Bearer%20eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOi[%E2%80%A6]GFsIn0.f0656mzmcRMSCywkbEptdd6JgkDfIqN0S9t-P1aPyt8", //token
+    "8158"] //userId
    );
 }
