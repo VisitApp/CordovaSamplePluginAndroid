8aeb1716c43424c8d2f33fe9d3e24614bce4e564
Chinmay <chinmaydash11@gmail.com>
2021-12-16 13:40:53 +0530

    Added status bar plugin

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -7,10 +7,19 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "cordova.plugins.CordavaFitnessPlugin"
       ]
+    },
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-whitelist": "1.3.5",
-    "info.android.plugin.fitness": "1.0.0"
+    "info.android.plugin.fitness": "1.0.0",
+    "cordova-plugin-statusbar": "3.0.0"
   };
 });
