14a7b0d83214e71a16c1a7edfd3e7fd7f410af70
chinmay <chinmaydash11@gmail.com>
2023-07-09 15:35:43 +0530

    removed unwanted code

--- CordavaFitnessPlugin/www/CordavaFitnessPlugin.js
+++ CordavaFitnessPlugin/www/CordavaFitnessPlugin.js
@@ -3,17 +3,3 @@ var exec = require("cordova/exec");
 exports.coolMethod = function (arg0, success, error) {
   exec(success, error, "CordavaFitnessPlugin", "coolMethod", [arg0]);
 };
-
-function plugin() {}
-
-plugin.prototype.new_activity = function () {
-  exec(
-    function (res) {},
-    function (err) {},
-    "PluginName",
-    "new_activity",
-    []
-  );
-};
-
-module.exports = new plugin();
--- platforms/android/app/src/main/assets/www/plugins/info.android.plugin.fitness/www/CordavaFitnessPlugin.js
+++ platforms/android/app/src/main/assets/www/plugins/info.android.plugin.fitness/www/CordavaFitnessPlugin.js
@@ -5,18 +5,4 @@ exports.coolMethod = function (arg0, success, error) {
   exec(success, error, "CordavaFitnessPlugin", "coolMethod", [arg0]);
 };
 
-function plugin() {}
-
-plugin.prototype.new_activity = function () {
-  exec(
-    function (res) {},
-    function (err) {},
-    "PluginName",
-    "new_activity",
-    []
-  );
-};
-
-module.exports = new plugin();
-
 });
