55b015a1c5b0cf4be951485ad1d336234b5f2414
chinmay <chinmaydash11@gmail.com>
2024-10-06 23:51:33 +0530

    created new project and tested health connect changes

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,22 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [
     {
-      "id": "cordova-plugin-splashscreen.SplashScreen",
-      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-      "pluginId": "cordova-plugin-splashscreen",
-      "clobbers": [
-        "navigator.splashscreen"
-      ]
-    },
-    {
-      "id": "cordova-plugin-statusbar.statusbar",
-      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-      "pluginId": "cordova-plugin-statusbar",
-      "clobbers": [
-        "window.StatusBar"
-      ]
-    },
-    {
       "id": "info.plugin.fitness.CordovaFitnessPlugin",
       "file": "plugins/info.plugin.fitness/www/CordovaFitnessPlugin.js",
       "pluginId": "info.plugin.fitness",
@@ -26,8 +10,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-splashscreen": "6.0.0",
-    "cordova-plugin-statusbar": "3.0.0",
     "info.plugin.fitness": "1.0.0"
   };
 });
